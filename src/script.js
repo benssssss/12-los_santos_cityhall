eedce019c0802db1df8c79f5dbbdabb993dd1f26
Bens <Jefferson.b3ntz@gmail.com>
2025-02-12 15:26:54 -0300

    arrow down (CSS) na home page implementada | scroll to top implementada (JS)

---
+++ src/script.js
@@ -0,0 +1,31 @@
+/* ------------ scrollToTop ---------- */
+
+let mybutton = document.getElementById("myBtn");
+// select button
+
+window.onscroll = function() {scrollFunction()};
+// add scroll event listener
+
+// define scroll function
+function scrollFunction() {
+
+  // check if the user scrolled more than 20px
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+    // if yes apply display block
+
+  } else {
+    mybutton.style.display = "none";
+    // if no apply display none
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+mybutton.addEventListener("click", topFunction);
+// add a listener to the mybutton and run topFunction on click
+
+/* ------------- Slider ------------ */
