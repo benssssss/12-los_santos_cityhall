084a762c5499f2f9f51e9e5a2db6fa0693c8cf47
Bens <Jefferson.b3ntz@gmail.com>
2025-02-13 11:38:36 -0300

    responsivo 350px da homepage | mobilepage pronto

---
+++ src/js/scriptslider.js
@@ -0,0 +1,61 @@
+// ---------------- progSlider --------------
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carouselItems = document.querySelectorAll('.carousel-item');
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    carouselItems.forEach(item => {
+      item.style.display = 'none';
+    });
+    carouselItems[index].style.display = 'block';
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % carouselItems.length;
+    showSlide(currentIndex);
+  }
+
+  function previousSlide() {
+    currentIndex = (currentIndex - 1 + carouselItems.length) % carouselItems.length;
+    showSlide(currentIndex);
+  }
+
+  // Show the first slide initially
+  showSlide(currentIndex);
+
+  // Set up event listeners for next and previous buttons
+  document.getElementById('nextBtn').addEventListener('click', nextSlide);
+  document.getElementById('prevBtn').addEventListener('click', previousSlide);
+});
+
+// ---------------- citySlider --------------
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carouselItems1 = document.querySelectorAll('.carousel-item-1');
+  let currentIndex = 0;
+
+  function showSlide(index) {
+    carouselItems1.forEach(item => {
+      item.style.display = 'none';
+    });
+    carouselItems1[index].style.display = 'block';
+  }
+
+  function nextSlide() {
+    currentIndex = (currentIndex + 1) % carouselItems1.length;
+    showSlide(currentIndex);
+  }
+
+  function previousSlide() {
+    currentIndex = (currentIndex - 1 + carouselItems1.length) % carouselItems1.length;
+    showSlide(currentIndex);
+  }
+
+  // Show the first slide initially
+  showSlide(currentIndex);
+
+  // Set up event listeners for next and previous buttons
+  document.getElementById('nextBtn1').addEventListener('click', nextSlide);
+  document.getElementById('prevBtn1').addEventListener('click', previousSlide);
+});
